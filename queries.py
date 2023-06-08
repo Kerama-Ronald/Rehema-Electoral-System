fa5305efec9f4f6871559c51c32692a53ec80b99
Ronald Kerama <keramaronald11@gmail.com>
2023-06-08 21:09:13 +0300

    Debugging error messages

--- queries.py
+++ queries.py
@@ -22,8 +22,8 @@ def get_contestants():
             'Category_id': contestant.Category_id,
             'Form': contestant.Form,
             'Stream': contestant.Stream,
-            'Grade_id': contestant.Grade_id,
-            'Fees_id': contestant.Fees_id
+            #'Grade_id': contestant.Grade_id,
+            #'Fees_id': contestant.Fees_id
         }
         contestants_list.append(contestant_dict)
     
@@ -44,22 +44,22 @@ def get_categories():
     
     return categories_list
 
-def get_winners():
-    winners = session.query(Contestant).join(Result).filter(Result.Percentage_votes == 100).all()
-    winners_list = []
+# def get_winners():
+#    # winners = session.query(Contestant).join(Result).filter(Result.Percentage_votes == 100).all()
+#     winners_list = []
     
-    for winner in winners:
-        winner_dict = {
-            'id': winner.id,
-            'First_Name': winner.First_Name,
-            'Last_Name': winner.Last_Name,
-            'Gender': winner.Gender,
-            'Category_id': winner.Category_id,
-            'Form': winner.Form,
-            'Stream': winner.Stream,
-            'Grade_id': winner.Grade_id,
-            'Fees_id': winner.Fees_id
-        }
-        winners_list.append(winner_dict)
+#     for winner in winners:
+#         winner_dict = {
+#             'id': winner.id,
+#             'First_Name': winner.First_Name,
+#             'Last_Name': winner.Last_Name,
+#             'Gender': winner.Gender,
+#             'Category_id': winner.Category_id,
+#             'Form': winner.Form,
+#             'Stream': winner.Stream,
+#             # 'Grade_id': winner.Grade_id,
+#             #'Fees_id': winner.Fees_id
+#         }
+#         winners_list.append(winner_dict)
     
-    return winners_list
+#     return winners_list
--- seed_data.py
+++ seed_data.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from database import Base, Contestant, Category, Grade, Fee
+from database import Base, Contestant, Category, Fee
 
 
 engine = create_engine("sqlite:///rehema.db")
@@ -14,8 +14,8 @@ def seed_data():
     category1 = Category(School_level='Primary', Form_level='Upper Primary', Class_level='Class 7')
     category2 = Category(School_level='Secondary', Form_level='High School', Class_level='Form 3')
 
-    grade1 = Grade(Student_Grade='B')
-    grade2 = Grade(Student_Grade='C')
+   # grade1 = Grade(Student_Grade='B')
+    #grade2 = Grade(Student_Grade='C')
 
     fee1 = Fee(Balance=0)
     fee2 = Fee(Balance=100)
@@ -23,5 +23,5 @@ def seed_data():
     contestant1 = Contestant(First_Name='Faith', Last_Name='Kilonzi', Gender='Male', Form='7A', Stream='S')
     contestant2 = Contestant(First_Name='Raphael', Last_Name='Katana', Gender='Female',Form='3B', Stream='T')
 
-    session.add_all([category1, category2, grade1, grade2, fee1, fee2, contestant1, contestant2])
+    session.add_all([category1, category2,fee1, fee2, contestant1, contestant2])
     session.commit()
