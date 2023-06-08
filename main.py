c86acbb3aceb7939274c7d4d3d2065f8eef2599d
Ronald Kerama <keramaronald11@gmail.com>
2023-06-08 23:18:29 +0300

    done

--- main.py
+++ main.py
@@ -1,26 +1,10 @@
-from queries import get_contestants, get_categories, get_winners
-from reports import generate_voters_report, generate_contestants_report ,generate_winners_report
-from seed_data import seed_data
-seed_data()
+from menu import handle_menu_choice, show_menu
 
-voters_report = generate_voters_report()
-contestants_report = generate_contestants_report()
-winners_report = generate_winners_report()
-
-print("Voters Report:")
-for entry in voters_report:
-    for key, value in entry.items():
-        print(f"{key}: {value}")
-    print()
-
-print("Contestants Report:")
-for entry in contestants_report:
-    for key, value in entry.items():
-        print(f"{key}: {value}")
-    print()
-
-print("Winners Report:")
-for entry in winners_report:
-    for key, value in entry.items():
-        print(f"{key}: {value}")
-    print()
+if __name__ == '__main__':
+    while True:
+        show_menu()
+        try:
+            choice = int(input("Enter your choice: "))
+            handle_menu_choice(choice)
+        except ValueError:
+            print("Invalid choice. Please try again.")
--- menu.py
+++ menu.py
@@ -1,15 +1,16 @@
-from ctypes import HRESULT
 from unittest import result
 from flask import sessions
-from database import Category, Contestant, Session
+from database import Category, Contestant, Result, session
+from seed_data import seed_data
 
 
 def show_menu():
-    print("Welcome to Rehema Election System!")
+    print("\nWelcome to Rehema Election System!")
     print("1. View Contestants")
     print("2. View Categories")
     print("3. View Winners")
-    print("4. Exit")
+    print("4. Seed Data (Developer mode)")
+    print("5. Exit")
 
 def handle_menu_choice(choice):
     if choice == 1:
@@ -19,34 +20,28 @@ def handle_menu_choice(choice):
     elif choice == 3:
         view_winners()
     elif choice == 4:
+        seed_data()
+    elif choice == 5:
         exit_program()
     else:
         print("Invalid choice. Please try again.")
 
 def view_contestants():
-    contestants = Session.query(Contestant).all()
+    contestants = session.query(Contestant).all()
     for contestant in contestants:
         print(f"ID: {contestant.id}, Name: {contestant.First_Name} {contestant.Last_Name}")
 
 def view_categories():
-    categories = sessions.query(Category).all()
+    categories = session.query(Category).all()
     for category in categories:
         print(f"ID: {category.id}, Level: {category.School_level}, Form Level: {category.Form_level}")
 
 def view_winners():
-    winners = Session.query(Contestant).join(HRESULT).filter(result.Percentage_votes == 100).all()
+    winners = session.query(Contestant).join(Result).filter(Result.Percentage_votes == 100).all()
     for winner in winners:
         print(f"ID: {winner.id}, Name: {winner.First_Name} {winner.Last_Name}")
 
 def exit_program():
-    Session.close()
+    session.close()
     print("Thank you for using the Election System. Goodbye!")
-
-def main():
-    show_menu()
-    choice = int(input("Enter your choice: "))
-    handle_menu_choice(choice)
-
-if __name__ == '__main__':
-    main()
-
+    exit(0)
