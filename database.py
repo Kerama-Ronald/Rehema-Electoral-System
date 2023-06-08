7650f377a7950c0ef0ba804f91b657f3bc083b32
Ronald Kerama <keramaronald11@gmail.com>
2023-06-08 13:42:07 +0300

    Debugging

--- database.py
+++ database.py
@@ -1,7 +1,6 @@
 from sqlalchemy import Column, String, Integer, ForeignKey
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine
 
 Base = declarative_base()
 
--- seed_data.py
+++ seed_data.py
@@ -17,8 +17,8 @@ def seed_data():
     grade1 = Grade(Student_Grade='B')
     grade2 = Grade(Student_Grade='C')
 
-    fee1 = Fees(Fees_paid=0)
-    fee2 = Fees(Fees_paid=100)
+    fee1 = Fees(Balance=0)
+    fee2 = Fees(Balance=100)
 
     contestant1 = Contestant(First_Name='Faith', Last_Name='Kilonzi', Gender='Male', Category=category1, Form='7A', Stream='S', Grade=grade1, Fees=fee1)
     contestant2 = Contestant(First_Name='Raphael', Last_Name='Katana', Gender='Female', Category=category2, Form='3B', Stream='T', Grade=grade2, Fees=fee2)
